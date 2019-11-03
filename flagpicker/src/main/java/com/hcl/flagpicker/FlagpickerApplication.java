dc0b380f05efd327ff3458c76aad564eb43aea2c
prakash <prakash@localhost>
2019-11-03 15:27:07 -0800

    Initial commit

---
+++ flagpicker/src/main/java/com/hcl/flagpicker/FlagpickerApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.flagpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagpickerApplication.class, args);
+	}
+
+}
